7196a8f57b04c53a3d2fc5ba8d5d220f547e3ebb
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-12-05 22:03:19 +0000

    Fix multiplication mix-up

---
+++ lean/natural_number_game/advanced_multiplication/4.lean
@@ -0,0 +1,32 @@
+lemma mul_left_comm (a b c : mynat) : a * (b * c) = b * (a * c) :=
+  -- + revert b because the website said so. I still don't /really/ understand
+  --   what revert is for, ie how you're supposed to spot that you should use
+  --   it.
+  -- + I *think* this works because it gives us a hypothesis with
+  --   "forall b" later, which is nicer than just having a hypothesis with "b"
+  --   in. It sort of sneaks b around the induction.
+
+  -- induction on c, because a didn't seem to work.
+  revert b,
+  induction c with n hn,
+  -- base case of induction: want to show
+  -- ∀ (b : mynat), a * b = a * 0 → b = 0
+  -- which is basically just mul_eq_zero_iff
+  intro b,
+  rw [mul_zero, mul_eq_zero_iff],
+  cc,
+
+  -- then also induct on b (?)
+  intros b h,
+  induction b with m hm,
+
+  symmetry at h,
+  rw [mul_zero, mul_eq_zero_iff] at h,
+  have hnz := succ_ne_zero n,
+  cc,
+
+  rw succ_eq_succ_iff,
+  apply hn,
+  repeat {rw mul_succ at h},
+  exact add_right_cancel _ a _ h,
+end
---
+++ lean/natural_number_game/lean_path.sh
@@ -0,0 +1,1 @@
+alias lean='LEAN_PATH=/home/izaak/fun/academic/lean/natural_number_game/src:/home/izaak/builds/lean/lean-3.4.2-linux/lib/lean/library:/home/izaak/builds/mathlib/src lean'
